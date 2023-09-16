89c2a252ecd42c84a309dc0d75ac13c937bfa966
Danny Solomon N <ndanny366@gmail.com>
2023-09-16 14:35:07 +0530

    Added a home page with API explanation

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const mongodb = require("./config/mongoose");
 // For parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
+//setting ejs as view engine
+app.set("view engine", "ejs");
+
+//telling express to look into the views folder for the ejs files
+app.set("views", "./views");
+
 //telling express to use express router
 // this should be only after ejs & layouts
 app.use("/", expressRouter);
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,18 @@ const router = express.Router();
 const questionController = require("../controllers/question_controller");
 const optionController = require("../controllers/option_controller");
 
+//route explaining the api
+router.get("/", (req, res) => {
+  res.render("home");
+});
+
+//route for all question related
 router.get("/questions", questionController.questionsView); //to view all questions
 router.get("/questions/:question_id", questionController.questionView); //view a question
 router.post("/questions/create", questionController.questionCreate); //create a question
 router.get("/questions/:question_id/delete", questionController.questionDelete); //delete a question
 
+//route for all option related
 router.post(
   "/questions/:question_id/options/create",
   optionController.optionCreate
